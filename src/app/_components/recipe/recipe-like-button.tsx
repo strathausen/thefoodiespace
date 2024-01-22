b0fa60e3bdb107f0625b8591f5ed6b8148c2c9c3
Philipp Strathausen <strathausen@gmail.com>
2024-01-22 13:56:53 +0100

    remove ugly refresh when liking

--- src/app/_components/recipe/recipe-like-button.tsx
+++ src/app/_components/recipe/recipe-like-button.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { api } from "@/trpc/react";
-import { useRouter } from "next/navigation";
 import { FaHeart, FaRegHeart } from "react-icons/fa6";
 import { PulseCounter } from "ui";
 
@@ -11,7 +10,6 @@ type Props = {
 };
 
 export const RecipeLikeButton = (props: Props) => {
-  const router = useRouter();
   const like = api.recipe.like.useMutation();
   const unlike = api.recipe.unlike.useMutation();
 
@@ -21,8 +19,6 @@ export const RecipeLikeButton = (props: Props) => {
     } else {
       await like.mutateAsync({ id: props.recipeId });
     }
-    // TODO - this works, but it may be better to just update the like count in some hook or state
-    router.refresh();
   }
 
   return (
