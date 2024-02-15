24ac689d8a74fc68f988294e800d19bab20deb11
Philipp Strathausen <strathausen@gmail.com>
2024-02-15 21:32:48 +0100

    fix missing import

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { reviewUser } from "@/inngest/functions";
+import { reviewUser } from "@/inngest/review-user-function";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
