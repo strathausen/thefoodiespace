6a87ff38ebc10123d0bd7b6df878396c03ca0cf1
Philipp Strathausen <strathausen@gmail.com>
2024-02-19 16:22:34 +0100

    fix wrong import

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,2 +1,2 @@
-export { reviewUser } from "./review-user-function";
+export { reviewUserFunction } from "./review-user-function";
 export { reviewRecipeImages } from "./review-recipe-images-function";
